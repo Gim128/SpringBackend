d12db14cf3075d40132b6acd6ea509804eb85401
ican-graphics-01 <lankaican@gmail.com>
2022-12-15 17:22:13 +0530

    complted CRUD operations in SpringBoot

--- src/main/java/net/leolabs/springbackend/controller/EmployeeController.java
+++ src/main/java/net/leolabs/springbackend/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import net.leolabs.springbackend.execption.ResourceNotFoundException;
 import net.leolabs.springbackend.model.Employee;
 import net.leolabs.springbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,5 +34,28 @@ public class EmployeeController {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with this id" + id));
         return  ResponseEntity.ok(employee);
+    }
+
+//    update employee REST API
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updatedEMployee(@PathVariable("id") long id,@RequestBody Employee employeeDetails){
+        Employee updateEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id" + id));
+        updateEmployee.setFirstName(employeeDetails.getFirstName());
+        updateEmployee.setLastName(employeeDetails.getLastName());
+        updateEmployee.setEmailId(employeeDetails.getEmailId());
+
+        employeeRepository.save(updateEmployee);
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+//    delete Employee REST API
+    @DeleteMapping("{id}")
+    public  ResponseEntity<Employee> deleteEmployee(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exixstys with id" + id));
+
+        employeeRepository.delete(employee);
+
+        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
