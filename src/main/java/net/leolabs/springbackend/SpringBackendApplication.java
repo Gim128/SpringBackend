56e912aa769edf1684e23c2086a879cc4262512d
ican-graphics-01 <lankaican@gmail.com>
2022-12-15 16:54:25 +0530

    create employee & get Employee methods implement

--- src/main/java/net/leolabs/springbackend/SpringBackendApplication.java
+++ src/main/java/net/leolabs/springbackend/SpringBackendApplication.java
@@ -1,13 +1,36 @@
 package net.leolabs.springbackend;
 
+import net.leolabs.springbackend.model.Employee;
+import net.leolabs.springbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBackendApplication {
+public class SpringBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setFirstName("Gimhan");
+		employee.setLastName("pabasara");
+		employee.setEmailId("gimhanpabasarar4@gmail.com");
+		employeeRepository.save(employee);
+
+		Employee employee1 = new Employee();
+		employee1.setFirstName("pabsaraa");
+		employee1.setLastName("mung");
+		employee1.setEmailId("pabasaramung94@gmail.com");
+		employeeRepository.save(employee1);
+
+
+	}
 }
--- src/main/java/net/leolabs/springbackend/controller/EmployeeController.java
+++ src/main/java/net/leolabs/springbackend/controller/EmployeeController.java
@@ -1,19 +1,37 @@
 package net.leolabs.springbackend.controller;
 
+import net.leolabs.springbackend.execption.ResourceNotFoundException;
 import net.leolabs.springbackend.model.Employee;
 import net.leolabs.springbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @GetMapping
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
+    }
+
+//    build create employee REST API
+    @PostMapping
+    public Employee createEMployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+//    create get employee by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with this id" + id));
+        return  ResponseEntity.ok(employee);
     }
 }
