54e01620c44125d8352f96d07ad43856f646eda0
ican-graphics-01 <lankaican@gmail.com>
2022-12-15 15:58:11 +0530

    configure database in mysql

--- src/main/java/net/leolabs/springbackend/SpringBackendApplication.java
+++ src/main/java/net/leolabs/springbackend/SpringBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBackendApplication.c lass, args);
+		SpringApplication.run(SpringBackendApplication.class, args);
 	}
 
 }
