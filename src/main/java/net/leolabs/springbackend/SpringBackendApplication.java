d7a5836c3a1a03d3b65faa2518b4f9d72796d84d
ican-graphics-01 <lankaican@gmail.com>
2022-12-15 15:37:46 +0530

    init springBoot project

---
+++ src/main/java/net/leolabs/springbackend/SpringBackendApplication.java
@@ -0,0 +1,13 @@
+package net.leolabs.springbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApplication.c lass, args);
+	}
+
+}
---
+++ src/test/java/net/leolabs/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leolabs.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
